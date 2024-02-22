fa3f87d687bda1026f1afa2d38edc565e196aa55
Fabricio Lima <fabriciotrab2005@gmail.com>
2024-02-22 10:31:00 -0300

    feat: botao padrao (dumb component)

---
+++ src/app/shared/layout/default-button/default-button.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'default-button',
+  templateUrl: './default-button.component.html',
+  styleUrls: ['./default-button.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class DefaultButtonComponent {
+  @Input() value: string = '';
+  @Input() type: 'primary' | 'secondary' | 'cancel' = 'primary';
+  @Input() disabled: boolean = false;
+  @Input() icon: string = '';
+  @Input() class: string = '';
+
+  @Output() clickEvent: EventEmitter<null> = new EventEmitter();
+}

de5f7d53fb630ef84e54912840ccca710b8cb6ac
Fabricio Lima <fabriciotrab2005@gmail.com>
2024-02-24 16:01:56 -0300

    feat: confirm  dialog service

--- src/app/core/services/confirm-dialog.service.ts
+++ src/app/core/services/confirm-dialog.service.ts
@@ -2,29 +2,27 @@ import { Injectable } from '@angular/core';
 import Swal from 'sweetalert2';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConfirmDialogService {
   private readonly swalWithBootstrapButtons = Swal.mixin({
     customClass: {
-      confirmButton: 'btn btn-success',
-      cancelButton: 'btn btn-danger',
-      actions: 'justify-center',
+      confirmButton: 'btn btn--primary !w-auto',
+      cancelButton: 'btn btn--cancel !w-auto',
+      actions: 'flex justify-center gap-2',
     },
-    buttonsStyling: false
-  })
+    buttonsStyling: false,
+  });
 
-  constructor() { }
-
+  constructor() {}
 
   confirm(
-    title:string,
-    description:string,
+    title: string,
+    description: string,
     accept: Function,
     confirmButtonText?: string,
-    cancelButtonText?: string,
-
-    ){
+    cancelButtonText?: string
+  ) {
     this.swalWithBootstrapButtons.fire({
       title: title,
       text: description,
@@ -34,13 +32,14 @@ export class ConfirmDialogService {
       cancelButtonText: cancelButtonText || 'Cancelar',
       reverseButtons: false,
       preConfirm: () => {
-        return new Promise((resolve) =>{
-          resolve(accept())
-        })
-      }
+        return new Promise((resolve) => {
+          resolve(accept());
+        });
+      },
     });
-    const confirmButton = document.querySelector('.swal2-confirm') as HTMLElement;
+    const confirmButton = document.querySelector(
+      '.swal2-confirm'
+    ) as HTMLElement;
     confirmButton.style.marginRight = '2rem';
   }
-
 }
